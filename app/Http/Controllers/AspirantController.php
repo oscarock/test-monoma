4ff19e8087b13d825609574f5cc48ac2462a089c
Oscar Diaz <oscarock02@gmail.com>
2023-11-20 03:26:49 -0500

    implement policies

--- app/Http/Controllers/AspirantController.php
+++ app/Http/Controllers/AspirantController.php
@@ -4,19 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddAspirantRequest;
 use App\Models\Aspirant;
+use App\Models\User;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Cache;
 class AspirantController extends Controller
 {
 	public function index()
 	{
-		$aspirants = Aspirant::all();
-		return response()->json([
-			"meta" => [
-				'success' => true,
-				'errors' => []
-			],
-			"data" => $aspirants
-		]);
+		try {
+			$user = JWTAuth::parseToken()->authenticate();
+			$cacheKey = 'aspirants_' . $user->id;
+
+			$aspirants = Cache::remember($cacheKey, now()->addMinutes(10), function () use ($user) {
+				if ($user->role === "agent") {
+					return Aspirant::where('owner', $user->id)->get();
+				} else {
+					Gate::authorize('viewAny', [User::class, $user->role]);
+					return Aspirant::all();
+				}
+			});
+
+			return response()->json([
+				"meta" => [
+					'success' => true,
+					'errors' => []
+				],
+				"data" => $aspirants
+			]);
+		} catch (\Exception $e) {
+			return response()->json([
+				"meta" => [
+					'success' => false,
+					'errors' => 'Profile not authorized to perform this action'
+				],
+			], 401);
+		}
 	}
 
 	public function show($id)
@@ -41,23 +64,33 @@ class AspirantController extends Controller
 	}
 
 	public function store(AddAspirantRequest $request){
-		$user = JWTAuth::parseToken()->authenticate();
+		try {
+			$user = JWTAuth::parseToken()->authenticate();
+			Gate::authorize('create', [User::class, $user->role]);
 
-		$aspirant = Aspirant::create([
-			"name" => $request->name,
-			"source" => $request->source,
-			"owner" => $request->owner,
-			"created_at" => date('Y-m-d h:i:s'),
-			"created_by" => $user->id,
-		]);
-		if($aspirant->save()){
+			$aspirant = Aspirant::create([
+				"name" => $request->name,
+				"source" => $request->source,
+				"owner" => $request->owner,
+				"created_at" => date('Y-m-d h:i:s'),
+				"created_by" => $user->id,
+			]);
+			if($aspirant->save()){
+				return response()->json([
+					"meta" => [
+						'success' => true,
+						'errors' => []
+					],
+					"data" => $aspirant
+				], 201);
+			}
+		} catch (\Exception $e) {
 			return response()->json([
 				"meta" => [
-					'success' => true,
-					'errors' => []
+					'success' => false,
+					'errors' => 'Profile not authorized to perform this action'
 				],
-				"data" => $aspirant
-			], 201);
+			], 401);
 		}
 	}
 }
